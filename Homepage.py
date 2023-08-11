5d2119cbfd3c55797bfb20bc88ec3eb129eb2887
TomOlds <olds.tom@gmail.com>
2023-08-11 18:05:11 -0400

    Initial commit

---
+++ Homepage.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+#st.set_page_config(page_title = "multi-page app" )
+st.title("Main Page")
+
+#st.sidebar.title("Navigation")
+st.sidebar.success("Select a page above")
+
+my_variable = "From Homepage.py"
+st.write(my_variable)
---
+++ pages/Projects.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Projects Page")
+
+#share variable from Homepage.py
+from Homepage import my_variable
+
+st.write(my_variable)
